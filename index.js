e2269f4c66f88295d4d544913526ecb7da0baa6a
briansrt <brianstevenriofriotrochez@hotmail.com>
2024-10-20 23:28:35 -0500

    Creacion de promocion-back

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {urlencoded, json} = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const userRoutes = require('./routes/userRoutes.routes.js');
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(urlencoded({extended: true}))
+app.use(json())
+
+app.use(cors())
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+app.get('/user/login', (req, res) => {
+    res.send('¡Hola, login!');
+});
+
+app.listen(port, ()=>{
+    console.log(`listening at port http://localhost:${port}`);
+})
